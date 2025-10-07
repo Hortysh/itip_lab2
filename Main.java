7bbb84f558f0e084a22bdd9ca5b40a771fb5514c
User <user@example.com>
2025-10-08 01:18:34 +0300

    laba 2

---
+++ Main.java
@@ -0,0 +1,174 @@
+
+// Абстрактный базовый класс
+abstract class GeometricFigure {
+    private String name;
+    private String color;
+    private double volume;
+
+    // Статический счётчик созданных объектов
+    protected static int objectCount = 0;
+
+    // Конструктор по умолчанию
+    public GeometricFigure() {
+        this.name = "Неизвестная фигура";
+        this.color = "Белый";
+        this.volume = 0.0;
+        objectCount++;
+    }
+
+    // Конструктор с параметрами
+    public GeometricFigure(String name, String color) {
+        this.name = name;
+        this.color = color;
+        objectCount++;
+    }
+
+    // Абстрактные методы
+    public abstract double calculateVolume();
+    public abstract void displayInfo();
+
+    // Геттеры и сеттеры
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    public static int getObjectCount() {
+        return objectCount;
+    }
+}
+
+// Класс Шар
+class Sphere extends GeometricFigure {
+    private double radius;
+
+    public Sphere() {
+        super("Шар", "Белый");
+        this.radius = 1.0;
+    }
+
+    public Sphere(String color, double radius) {
+        super("Шар", color);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateVolume() {
+        double volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+        setVolume(volume);
+        return volume;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.printf("Фигура: %s, Цвет: %s, Радиус: %.2f, Объём: %.2f%n",
+                getName(), getColor(), radius, calculateVolume());
+    }
+}
+
+// Класс Параллелепипед
+class Parallelepiped extends GeometricFigure {
+    private double length;
+    private double width;
+    private double height;
+
+    public Parallelepiped() {
+        super("Параллелепипед", "Серый");
+        this.length = 1.0;
+        this.width = 1.0;
+        this.height = 1.0;
+    }
+
+    public Parallelepiped(String color, double length, double width, double height) {
+        super("Параллелепипед", color);
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double calculateVolume() {
+        double volume = length * width * height;
+        setVolume(volume);
+        return volume;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.printf("Фигура: %s, Цвет: %s, Размеры: %.2fx%.2fx%.2f, Объём: %.2f%n",
+                getName(), getColor(), length, width, height, calculateVolume());
+    }
+}
+
+// Класс Цилиндр
+class Cylinder extends GeometricFigure {
+    private double radius;
+    private double height;
+
+    public Cylinder() {
+        super("Цилиндр", "Синий");
+        this.radius = 1.0;
+        this.height = 1.0;
+    }
+
+    public Cylinder(String color, double radius, double height) {
+        super("Цилиндр", color);
+        this.radius = radius;
+        this.height = height;
+    }
+
+    @Override
+    public double calculateVolume() {
+        double volume = Math.PI * Math.pow(radius, 2) * height;
+        setVolume(volume);
+        return volume;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.printf("Фигура: %s, Цвет: %s, Радиус: %.2f, Высота: %.2f, Объём: %.2f%n",
+                getName(), getColor(), radius, height, calculateVolume());
+    }
+}
+
+// Демонстрация работы
+public class Main {
+    public static void main(String[] args) {
+        GeometricFigure sphere = new Sphere("Красный", 3.5);
+        GeometricFigure parallelepiped = new Parallelepiped("Зелёный", 2, 3, 4);
+        GeometricFigure cylinder = new Cylinder("Жёлтый", 2.5, 5);
+
+        sphere.displayInfo();
+        parallelepiped.displayInfo();
+        cylinder.displayInfo();
+
+        System.out.println("\nВсего создано геометрических фигур: " + GeometricFigure.getObjectCount());
+    }
+}
